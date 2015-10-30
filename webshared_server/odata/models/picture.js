95b3c118e0416b13d0a04fb15203781dbd63be37
weiyang <503945930@qq.com>
2015-10-30 13:29:55 +0800

    添加图片评论

--- webshared_server/odata/models/picture.js
+++ webshared_server/odata/models/picture.js
@@ -5,7 +5,7 @@ module.exports={
     user:String,//用户id
     activity:String,//活动id
     pictureUrl:String,//图片路径
-    statistics:{praise:{type:Number,default:0}},//点赞
+    statistics:{praise:{type:Number,default:0},comment:{type:Number,default:0}},//点赞
     creatInfo:{createTime: {type: Date, default: Date.now},perple:String},//创建信息  创建时间、创建人
     updateInfo:{createTime: {type: Date, default: Date.now},perple:String},//修改信息  修改时间、修改人
     states:Boolean//状态
